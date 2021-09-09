f056614c323998522c52c23cceeb7339e46d5b4a
DobychinVS <climb27@mail.ru>
2021-09-09 14:58:44 +0500

    Добавьте файлы проекта.

---
+++ Ecommerce/Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eccomerce.Models
+{
+    public class Cart
+    {
+        public int Ammount { get; set; }
+    }
+}
